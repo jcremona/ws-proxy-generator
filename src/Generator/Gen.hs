8c1aa0aec1f019ba91f8e55a497667f8ca2e75bf
jcremona <jcremona@laptop>
2018-09-26 16:17:55 -0300

    WIP: syntax

--- src/Generator/Gen.hs
+++ src/Generator/Gen.hs
@@ -1,5 +1,9 @@
+import StReader
+import Data.Char
+import Control.Monad
 --data DataType = DataType 
 --              {
+
 
 data DefFun = DefFun
             { name       :: String
@@ -8,13 +12,12 @@ data DefFun = DefFun
             }
 
 data Type = Single HType | Rec HType Type -- separar los tipos primitivos?? tiene sentido un String? se usaria para los tipos definidos por el usuario
-
 data HType = UserDefined String | Prim Primitive 
-
 data Primitive = TInt | TString
 
-data Expr = Call String [Expr] | Free String 
+data Expr = Call String [Expr] | Free String
 
+type TypedParams = [(String, Type)]
 
 --Call "sum" [Free "a", Free "b"]
 --"sum" => T "Int" (T "Int" (T "Int"))
@@ -48,5 +51,71 @@ prettyPrinterHType (UserDefined udtype) = udtype
 prettyPrinterHType (Prim TInt) = "Int"
 prettyPrinterHType (Prim TString) = "String"
 
---func = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "show" [Free "a", Free "b"])
+pp :: Maybe DefFun -> String
+pp Nothing = "Syntax error"
+pp (Just fun) = prettyPrinter fun
+
+--typeChecking :: DefFun -> StReader () TypedParams Type
+--typeChecking (Def name params body) = 
+
+-- VER PALABRAS RESERVADAS
+
+--typeCheckingExpr :: Expr -> StReader [(String, Type)] TypedParams Type -- PRIMERO NO DEBERIA BUSCAR LA FUNC ENTRE LOS PARAM??
+--typeCheckingExpr (Call name subexprs) = do params <- get
+--                                           case lookup name params of
+--                                                  Nothing -> throw
+--                                                  Just t -> t
+                                           
+ 
+                                           
+-- si esta entre los param, tiparlo
+-- si no, buscar entre las func
+--	si esta, tiparlo
+--	si no, hay que procesar la llamada
+
+
+--typeCheckingExpr (Free vble) =  lookupVble vble
+
+--check :: [Expr] -> Type -> Bool
+--check exps ty = foldl (\ exp -> typeCheckingExpr exp 
+
+
+lookupVble :: Eq key => key -> StReader s [(key, value)] value
+lookupVble vble = do params <- ask
+                     case lookup vble params of
+                         Nothing -> throwExc
+                         Just t -> return t
+
+
+class Monad m => MonadThrowable m where
+      throw :: String -> m a
+      guardT :: Bool -> a -> m a -- Agregar String para informar el error?
+
+instance MonadThrowable Maybe where
+      throw _ = Nothing
+      guardT = \x -> (guard x >>) . Just
+
+
+lower :: (MonadThrowable m) => String -> m String
+lower [] = throw "Invalid identifier (empty String)"
+lower (x:xs) = guardT (isLower x) (x:xs)
+
+allDifferent :: (Eq a) => [a] -> Bool
+allDifferent []     = True
+allDifferent (x:xs) = x `notElem` xs && allDifferent xs 
+
+syntaxAnalyzer :: (MonadThrowable m) => DefFun -> m DefFun
+syntaxAnalyzer fun = do fname <- lower . name $ fun
+                        ps <- guardT (allDifferent $ map fst params ++ [fname]) params
+                        mapM (lower . fst) ps
+                        return fun
+                     where params = parameters fun
+
+asx :: (MonadThrowable m) => [DefFun] -> m [DefFun]
+asx funcs = do mapM syntaxAnalyzer funcs
+               guardT (allDifferent $ map name funcs) funcs
+-- params podria ser Reader, y lista de func ya tipadas podria ser state
+
+func = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "show" [Free "a", Free "b"])
+--func2 = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "show" [Free "a", Free "b"])
 --typ = Rec (Prim TString) (Rec (Prim TInt) (Single $ Prim TInt))
---
+++ src/Generator/StReader.hs
@@ -0,0 +1,52 @@
+module StReader where
+
+import Control.Applicative (Applicative(..))
+import Control.Monad (liftM,ap)
+
+--newtype State s a = State { runState :: s -> (a, s) }
+--newtype Reader e a = Reader { runReader :: e -> a }
+
+newtype StReader s e a = StReader { runStReader :: s -> e -> Maybe (a,s) }
+
+instance Functor (StReader s e) where
+    fmap = liftM
+
+instance Applicative (StReader s e) where
+    pure = return
+    (<*>) = ap
+
+instance Monad (StReader s e) where
+    return x = StReader (\ s e -> Just (x, s) )
+    StReader t >>= f = StReader (\ st e -> case t st e of
+                                                Nothing -> Nothing
+                                                Just (a, st') -> runStReader (f a) st' e) 
+
+
+throwExc :: StReader s e a
+throwExc = StReader (\ _ _ -> Nothing)
+
+ask :: StReader s e e
+ask = StReader (\ st env -> Just (env, st))
+
+put :: s -> StReader s e ()
+put st = StReader (\ _ _ -> Just ((), st))
+
+get :: StReader s e s
+get = StReader (\ st _ -> Just (st, st)) 
+
+--(\ st -> let (a, st') = s st 
+--          in runState (f a) st'
+  
+
+-- f :: a -> State s b
+-- s :: s -> (a, s)
+-- runState :: State s a -> s -> (a,s)
+
+
+
+--    Reader env >>= f = (\e -> runReader (f (env e)) e) 
+
+
+-- f :: a -> Reader e b
+-- env :: e -> a
+
