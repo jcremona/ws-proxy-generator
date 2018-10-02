02e1f71e425e0b6ea685333439c865bd0753b6cf
jcremona <jcremona@laptop>
2018-10-02 12:22:53 -0300

    WIP: funciones externas

--- src/Generator/Gen.hs
+++ src/Generator/Gen.hs
@@ -1,6 +1,7 @@
 import StReader
 import Data.Char
 import Control.Monad
+import Control.Applicative
 --data DataType = DataType 
 --              {
 
@@ -9,13 +10,13 @@ data DefFun = DefFun
             { name       :: String
             , parameters :: [(String, Type)] -- un param puede ser una funcion, deberia poder usarse esa funcion localmente
             , body       :: Expr
-            }
+            } deriving Show
 
-data Type = Single HType | Rec HType Type -- separar los tipos primitivos?? tiene sentido un String? se usaria para los tipos definidos por el usuario
-data HType = UserDefined String | Prim Primitive 
-data Primitive = TInt | TString
+data Type = Single HType | Rec HType Type deriving Show -- separar los tipos primitivos?? tiene sentido un String? se usaria para los tipos definidos por el usuario
+data HType = UserDefined String | Prim Primitive deriving Show 
+data Primitive = TInt | TString deriving Show
 
-data Expr = Call String [Expr] | Free String
+data Expr = Call String [Expr] | Free String deriving Show
 
 type TypedParams = [(String, Type)]
 
@@ -87,7 +88,7 @@ lookupVble vble = do params <- ask
                          Just t -> return t
 
 
-class Monad m => MonadThrowable m where
+class (Monad m, Alternative m) => MonadThrowable m where
       throw :: String -> m a
       guardT :: Bool -> a -> m a -- Agregar String para informar el error?
 
@@ -113,9 +114,45 @@ syntaxAnalyzer fun = do fname <- lower . name $ fun
 
 asx :: (MonadThrowable m) => [DefFun] -> m [DefFun]
 asx funcs = do mapM syntaxAnalyzer funcs
-               guardT (allDifferent $ map name funcs) funcs
+               fnames <- guardT (allDifferent $ names) names -- DEBEN llamarse diferente a las func EXternas tmb????
+               foldM (kkp nodes) [] funcs
+               return funcs
+            where nodes = zip names [0..]
+                  names = map name funcs
+
+-- ver como meter las funciones externas
+
+
+
+kkp :: (MonadThrowable m) => [(String, Int)] -> [(Int,[Int])] -> DefFun -> m [(Int,[Int])]
+kkp xs ys fun = do node <- lookupT (name fun) xs
+                   kexpr node . body $ fun
+                where kexpr n (Call name' exprs) = (do dd <- (lookupT name' xs)
+                                                       return $ addT ys dd n)           ---         <|> 
+                      kexpr n (Free vble) = return ys                 
+
+-- PUEDO TENER UNA LLAMADA A UNA FUNC PASADA POR PARAMETRO, ACA FALLARIA!
+
+--f1 a = f2 2a
+--f3 a = f1 a
+
+
+--kexpr :: Expr -> Int
+
 -- params podria ser Reader, y lista de func ya tipadas podria ser state
 
-func = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "show" [Free "a", Free "b"])
+lookupT :: (MonadThrowable m, Eq a) => a -> [(a,b)] -> m b
+lookupT _ [] = throw $ "Unable to find this key"
+lookupT x ((y,z):ys) | x == y = return z
+                     | otherwise = lookupT x ys
+
+
+addT :: Eq a => [(a, [b])] -> a -> b -> [(a, [b])]
+addT [] a b = [(a, [b])]
+addT ((x,bs):ys) a b | x == a = [(x, b:bs)] ++ ys
+                     | otherwise = (x,bs) : addT ys a b
+
+
+func = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "sum" [Free "a", Free "b"])
 --func2 = DefFun "sum" [("a", Single $ Prim TInt), ("b", Single $ Prim TInt)] (Call "show" [Free "a", Free "b"])
 --typ = Rec (Prim TString) (Rec (Prim TInt) (Single $ Prim TInt))
